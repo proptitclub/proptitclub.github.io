7ca1cae66d3f5e012b6fd65e52ce1dda030fa37d
tuenguyen <tue2kptit@gmail.com>
2020-10-08 09:04:05 +0700

    add disqus

--- _site/assets/js/search-worker-9.0.3.js
+++ _site/assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-10-08T08:17:08+07:00';
+const INDEX_KEY = 'index--2020-10-08T08:59:28+07:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
--- _site/sw.js
+++ _site/sw.js
@@ -1,1 +1,1 @@
-importScripts("/assets/js/sw.js?t=2020-10-08T08:17:08+07:00");
+importScripts("/assets/js/sw.js?t=2020-10-08T08:59:28+07:00");
